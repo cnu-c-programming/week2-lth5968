44aea9e0f15fa965b328a73889d43c38a72c3898
lth5968 <lth5968@gmail.com>
2026-03-13 09:29:16 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -4,6 +4,18 @@ int main()
 {
     int a = 100;
     printf("int : %d\n", a);
+    printf("hex : %x\n", a);
+    printf("oct : %o\n", a);
+
+    char b = 'c';
+    printf("char : %c\n", b);
+
+    char* c = "hello world!";
+    printf("string : %s\n", c);
+
+    float d = 3.141592;
+    printf("float : %c\n", d);
+    printf("exp : %c\n", d);
 
     return 0;
 }
