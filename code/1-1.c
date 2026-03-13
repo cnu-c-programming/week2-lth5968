4066fcc2dd86faa2822a16d4b2340dd74ae56cb2
lth5968 <lth5968@gmail.com>
2026-03-13 09:34:45 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -2,18 +2,18 @@
 
 int main()
 {
-    int a = 100;
+    int var1 = 100;
     printf("int : %d\n", a);
     printf("hex : %x\n", a);
     printf("oct : %o\n", a);
 
-    char b = 'c';
+    char var2 = 'c';
     printf("char : %c\n", b);
 
-    char* c = "hello world!";
+    char* var3 = "hello world!";
     printf("string : %s\n", c);
 
-    float d = 3.141592;
+    float var4 = 3.141592;
     printf("float : %f\n", d);
     printf("exp : %d\n", d);
 
