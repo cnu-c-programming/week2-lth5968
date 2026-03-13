6232d7f0190920143c18156d29469c5daeb447b6
lth5968 <lth5968@gmail.com>
2026-03-13 09:44:57 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -2,7 +2,14 @@
 
 int main()
 {
+    int age;
+    char grade;
+    float gpa;
 
+    scanf("%d", &age);
+    scanf("%c", &grade);
+    scanf("%f", &gpa);
+    printf("%d %c %f\n", age, grade, gpa)
 
     return 0;
 }
