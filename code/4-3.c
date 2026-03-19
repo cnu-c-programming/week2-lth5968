0d3fbfceecdf4acd560164fa03666d952cc6d093
lth5968 <lth5968@gmail.com>
2026-03-19 22:05:53 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,7 +2,29 @@
 
 int main()
 {
+    int n;
+    scanf("%d", &n);
+
+    int isPrime = 1;
+
+    if (n <= 1)
+        isPrime = 0;
+    else
+    {
+        for (int i = 2; i * i <= n; i++)
+        {
+            if (n % i == 0)
+            {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+    if (isPrime)
+        printf("true\n");
+    else
+        printf("false\n");
 
     return 0;
 }
-
