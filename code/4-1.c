fff5c9aca71f7fc27842f238cb29b6b07e230ba1
lth5968 <lth5968@gmail.com>
2026-03-19 21:51:08 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -2,7 +2,23 @@
 
 int main()
 {
+    int a, b;
+    char op;
+
+    scanf("%d %d %c", &a, &b, &op);
+
+    int result;
+
+    if (op == '+')
+        result = a + b;
+    else if (op == '-')
+        result = a - b;
+    else if (op == '*')
+        result = a * b;
+    else if (op == '/')
+        result = a / b;
+
+    printf("%d\n", result);
 
     return 0;
 }
-
